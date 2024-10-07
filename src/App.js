ac5432c44ca011bf85580d3f399a98baa36488b7
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-10-07 17:45:23 +0500

    main

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Loading } from './Components/Loading';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // Start with loading true
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  async function fetchData() {
+    try {
+      const response = await axios.get('https://api.randomuser.me/?nat=PK&results=2');
+      setData(response.data?.results);
+      console.log("Data", response.data)
+      setLoading(false); // Set loading to false after data is fetched
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false); // Ensure loading is set to false even if there's an error
+    }
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetchData()
+  };
+
+  return (
+    <div className="App">
+      <h1>Data After Mapping</h1>
+      {loading ? (
+        <Loading />
+      ) : (
+
+        data.map((res, index) => (
+          <div key={index}>
+            <br />
+            <span>Gender: {res.gender}</span>
+            <br />
+            <span>{res.name.title}</span>
+            <br />
+            <span>{res.name.first} {res.name.last}</span>
+
+          </div>
+        ))
+
+      )}
+       <form onSubmit={handleSubmit}>
+              <input type='submit' value='load users' />
+            </form>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loading = ({message}) => {
+  return (
+    <div>
+        <h1>{message}</h1>
+    </div>
+  )
+}
